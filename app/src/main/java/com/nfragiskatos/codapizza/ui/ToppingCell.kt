0d098d2ae2b8a2cd08604678402147f5349da441
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-08-25 13:01:38 -0700

    - added typography, and padding - added modifier parameter to composable

--- app/src/main/java/com/nfragiskatos/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/nfragiskatos/codapizza/ui/ToppingCell.kt
@@ -1,13 +1,20 @@
 package com.nfragiskatos.codapizza.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Checkbox
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nfragiskatos.codapizza.model.Topping
 import com.nfragiskatos.codapizza.model.ToppingPlacement
 
@@ -35,16 +42,28 @@ private fun ToppingCellPreviewOnLeftHalf() {
 fun ToppingCell(
     topping: Topping,
     placement: ToppingPlacement?,
+    modifier: Modifier = Modifier,
     onClickTopping: () -> Unit
 ) {
     Row (
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .clickable { onClickTopping() }
+            .padding(vertical = 4.dp, horizontal = 16.dp)
     ) {
         Checkbox(checked = true, onCheckedChange = {/* TODO */})
-        Column {
-            Text(text = stringResource(id = topping.toppingName))
+        Column(
+            modifier = Modifier.weight(1f, fill = true).padding(start = 4.dp)
+        ) {
+            Text(
+                text = stringResource(id = topping.toppingName),
+                style = MaterialTheme.typography.body1
+            )
             if (placement != null) {
-                Text(text = stringResource(id = placement.label))
+                Text(
+                    text = stringResource(id = placement.label),
+                    style = MaterialTheme.typography.body2
+                )
             }
         }
     }
