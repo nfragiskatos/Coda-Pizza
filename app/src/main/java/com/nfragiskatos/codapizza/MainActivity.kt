b2aa8ec1c13efbffa265317cf0c1ee6768eb1257
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-08-25 09:28:22 -0700

    - removed view layout - in MainActivity, switched setContentView to use setContent and added basic hello world composable for test

--- app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
@@ -2,10 +2,14 @@ package com.nfragiskatos.codapizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { 
+            Text(text = "Hello World!")
+        }
     }
 }
