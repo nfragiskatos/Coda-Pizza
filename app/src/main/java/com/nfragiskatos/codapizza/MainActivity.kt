22b1b9bf571560f174cbae2266139ad1f9ea546f
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-08-25 11:59:54 -0700

    - added parameters to ToppingCell composable to accept Topping, ToppingPlacement enums, and an onclick

--- app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.nfragiskatos.codapizza.model.Topping
+import com.nfragiskatos.codapizza.model.ToppingPlacement
 import com.nfragiskatos.codapizza.ui.ToppingCell
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToppingCell()
+            ToppingCell(
+                topping = Topping.Pepperoni,
+                placement = ToppingPlacement.Left,
+                onClickTopping = {}
+            )
         }
     }
 }
--- app/src/main/java/com/nfragiskatos/codapizza/ui/ToppingCell.kt
+++ app/src/main/java/com/nfragiskatos/codapizza/ui/ToppingCell.kt
@@ -5,20 +5,31 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.nfragiskatos.codapizza.model.Topping
+import com.nfragiskatos.codapizza.model.ToppingPlacement
 
 @Preview
 @Composable
 private fun ToppingCellPreview() {
-    ToppingCell()
+    ToppingCell(
+        topping = Topping.Pepperoni,
+        placement = ToppingPlacement.Left,
+        onClickTopping = {}
+    )
 }
 
 @Composable
-fun ToppingCell() {
+fun ToppingCell(
+    topping: Topping,
+    placement: ToppingPlacement?,
+    onClickTopping: () -> Unit
+) {
     Row {
         Checkbox(checked = true, onCheckedChange = {/* TODO */})
         Column {
-            Text(text = "Pineapple")
+            Text(text = stringResource(id = topping.toppingName))
             Text(text = "Whole Pizza")
         }
     }
