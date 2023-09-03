19eece1b9b32dd57dffad9fd215d0fbb1667b964
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-09-03 16:39:39 -0700

    - added custom AppTheme - removed dead code

--- app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/codapizza/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.nfragiskatos.codapizza
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import com.nfragiskatos.codapizza.model.Topping
-import com.nfragiskatos.codapizza.model.ToppingPlacement
+import androidx.appcompat.app.AppCompatActivity
+import com.nfragiskatos.codapizza.ui.AppTheme
 import com.nfragiskatos.codapizza.ui.PizzaBuilderScreen
-import com.nfragiskatos.codapizza.ui.ToppingCell
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PizzaBuilderScreen()
+            AppTheme {
+                PizzaBuilderScreen()
+            }
         }
     }
 }
